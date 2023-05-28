a4b9bec203b48a6d42096ffbbf794a929fd8b9d5
adenugbamichael <adenugbamicke@gmail.com>
2023-05-28 12:59:21 +0100

    Context Provider

---
+++ src/contexts/BudgetsContext.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useContext } from "react";
+import { v4 as uuidV4 } from "uuid";
+
+const BudgetsContext = React.createContext();
+
+export function useBudgets() {
+  return useContext(BudgetsContext);
+}
+
+// {
+//   id:
+//   name:
+//   max
+// }
+// {
+//   id:
+//   budgetId:
+//   amount:
+//   description:
+// }
+
+export const BudgetsProvider = ({ children }) => {
+  const [budgets, setBudgets] = useState([]);
+  const [expenses, setExpenses] = useState([]);
+
+  function getBudgetExpenses(budgetId) {
+    return expenses.filter((expense) => expense.budgetId === budgetId);
+  }
+  function addExpense({ description, amount, budgetId }) {
+    setExpenses((prevExpenses) => {
+      return [...prevExpenses, { id: uuidV4(), description, amount, budgetId }];
+    });
+  }
+  function addBudget({ name, max }) {
+    setBudgets((prevBudgets) => {
+      if (prevBudgets.find((budget) => budget.name === name)) {
+        return prevBudgets;
+      }
+      return [...prevBudgets, { id: uuidV4(), name, max }];
+    });
+  }
+  function deleteBudget(id) {}
+  function deleteExpense(id) {}
+
+  return (
+    <BudgetsContext.Provider
+      value={{
+        budgets,
+        expenses,
+        getBudgetExpenses,
+        addExpense,
+        addBudget,
+        deleteBudget,
+        deleteExpense,
+      }}
+    >
+      {children}
+    </BudgetsContext.Provider>
+  );
+};
